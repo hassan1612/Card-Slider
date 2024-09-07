94a5d13722aef515208e0cb3c0c2d41f54f9a1c6
Hassan Shaikh <160763396+hassan1612@users.noreply.github.com>
2024-09-07 14:39:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+new Swiper('.card-wrap', {
+    spaceBetween: 20,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
